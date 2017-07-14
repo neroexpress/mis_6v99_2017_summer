261454ac444a79bd46ac742d3a0dcdc7ea840ed4
Narender Kumar <narenderkumar@cometnet-10-21-11-180.utdallas.edu>
2017-07-14 18:51:08 -0500

    code added

--- lab_02/demo_numpy.py
+++ lab_02/demo_numpy.py
@@ -1,2 +1,15 @@
 import numpy as np
 a = np.arange(15).reshape(3,5)
+
+#Print the array
+print(a)
+#Print the shape
+print(a.shape)
+#Print the size
+print(a.size)
+#Print the item size
+print(a.itemsize)
+#Print the number of dimensions
+print(a.size)
+#Print the data type name (not the data type)
+print(type(a.dtype))
