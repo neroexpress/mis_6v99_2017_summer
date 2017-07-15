9127146369c4556da016ca1597b7ba5096b2d4f5
Narender Kumar <narenderkumar@cometnet-10-21-11-180.utdallas.edu>
2017-07-14 19:05:54 -0500

    code added

--- lab_02/demo_numpy.py
+++ lab_02/demo_numpy.py
@@ -4,12 +4,12 @@ a = np.arange(15).reshape(3,5)
 file_name = "demo_numpy.txt"
 
 list_of_values = list()
-list_of_values.extend((a,a.shape,a.size,a.itemsize,a.size,type(a.dtype)))
+list_of_values.extend((a,a.shape,a.size,a.itemsize,a.ndim,type(a.dtype)))
 
 def print_to_textfile(fn,list_of_values):
 	with open(fn,'wt') as f:
 		for s in list_of_values:
-			print(s)
+			#print(s)
 			print(s, file=f)
 
 print_to_textfile(file_name,list_of_values)
