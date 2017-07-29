41ece52ad38c9fca35cf2af023395ddde02822bd
Narender Kumar <narenderkumar@cometnet-10-21-29-88.utdallas.edu>
2017-07-28 19:11:47 -0500

    Python file added

--- assignment_03/market_basket_analytics.py
+++ assignment_03/market_basket_analytics.py
@@ -2,16 +2,23 @@
 
 import requests
 
-
 training_url  = "http://kevincrook.com/utd/market_basket_training.txt"
 test_url = "http://kevincrook.com/utd/market_basket_test.txt"
+training_file="market_basket_training.txt"
+test_file = "market_basket_test.txt"
 
-r_training = requests.get(training_url)
-trainingf = open("training_data.txt","wb")
-trainingf.write(r_training.content)
-trainingf.close()
+def get_training_set(training_url,training_file):
+	r_training = requests.get(training_url)
+	trainingf = open(training_file,"wb")
+	trainingf.write(r_training.content)
+	trainingf.close()
 
-r_test = requests.get(test_url)
-testf = open("test_data.txt","wb")
-testf.write(r_test.content)
-testf.close()
+def get_test_data(test_url,test_file):
+	r_test = requests.get(test_url)
+	testf = open(test_file,"wb")
+	testf.write(r_test.content)
+	testf.close()
+
+#get_training_set(training_url,training_file)
+#get_test_data(test_url,test_file)
+
